2b3ea8e5a6e4766c487e40de2e76c3ec35734d78
Joooshutup <149413814+Joooshutup@users.noreply.github.com>
2025-06-09 13:50:32 +0800

    cheatsheet

---
+++ 02_control_structures/10_switch_cheatsheet.c
@@ -0,0 +1,15 @@
+// Switch case default的應用
+switch (變數) {
+    case 值1:
+        // 當變數 == 值1 時執行
+        break;
+    case 值2:
+        // 當變數 == 值2 時執行
+        break;
+    case 值3:
+        // 當變數 == 值3 時執行
+        break;
+    default:
+        // 當變數不符合上面任何 case 時執行
+        break;
+}
