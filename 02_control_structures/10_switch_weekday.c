8476b90ea61b3af8a918923d25bc0058d1b7de55
Joooshutup <149413814+Joooshutup@users.noreply.github.com>
2025-06-09 13:18:54 +0800

    switch weekday

--- 02_control_structures/10_switch_weekday.c
+++ 02_control_structures/10_switch_weekday.c
@@ -1,3 +1,21 @@
+// Switch case default的應用
+/*
+switch (變數) {
+    case 值1:
+        // 當變數 == 值1 時執行
+        break;
+    case 值2:
+        // 當變數 == 值2 時執行
+        break;
+    case 值3:
+        // 當變數 == 值3 時執行
+        break;
+    default:
+        // 當變數不符合上面任何 case 時執行
+        break;
+}
+*/
+
 // 星期幾？
 #include <stdio.h>
 
